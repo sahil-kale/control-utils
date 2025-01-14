f797b127fc684a40133cc69459affa9fbb423438
Sahil Kale <sahil.kale1@gmail.com>
2025-01-14 16:59:23 -0500

    Add interpolation table module

---
+++ control_utils_lib/inc/interpolation_table.h
@@ -0,0 +1,52 @@
+#ifndef INTERPOLATION_TABLE_H
+#define INTERPOLATION_TABLE_H
+
+#include <stdbool.h>
+#include <stddef.h>
+
+#include "util.h"
+
+typedef struct {
+    float *x;     /// X values - MUST BE ASCENDING (lookup value key)
+    float *y;     /// Y values (lookup value pair)
+    size_t size;  /// Size of the table
+
+    float *slopeBuffer;  /// Buffer to store the slope values. slopeBuffer[i] is the slope on the interval between [x[i], x[i+1])
+    size_t slopeBufferSize;  /// Size of the slope buffer
+
+    bool initialized;  /// Flag to indicate if the table has been initialized
+} control_utils_interpolation_table_t;
+
+#define CONTROL_UTILS_DECLARE_INTERPOLATION_TABLE(name, ascending_x_values, y_values)        \
+    CONTROL_UTILS_STATIC_ASSERT(sizeof(ascending_x_values) == sizeof(y_values),              \
+                                "X and Y value arrays should have the same size");           \
+    static float slopeBuffer_##name[(sizeof(ascending_x_values) / sizeof(float) - 1)] = {0}; \
+    static control_utils_interpolation_table_t name = {                                      \
+        .x = ascending_x_values,                                                             \
+        .y = y_values,                                                                       \
+        .size = sizeof(ascending_x_values) / sizeof(float),                                  \
+        .slopeBuffer = slopeBuffer_##name,                                                   \
+        .slopeBufferSize = sizeof(slopeBuffer_##name) / sizeof(float),                       \
+        .initialized = false,                                                                \
+    };
+
+/**
+ * @brief Initialize the interpolation table
+ *
+ * @param table Pointer to the interpolation table
+ *
+ * @return true if the interpolation table was initialized successfully, false otherwise
+ */
+bool control_utils_interpolation_table_init(control_utils_interpolation_table_t *const table);
+
+/**
+ * @brief Interpolate a value from the interpolation table
+ *
+ * @param table Pointer to the interpolation table
+ * @param x The x value to interpolate
+ * @param y Pointer to the interpolated y value - will be set if the interpolation is successful
+ */
+bool control_utils_interpolation_table_interpolate(control_utils_interpolation_table_t const *const table, float x,
+                                                   float *const y);
+
+#endif  // INTERPOLATION_TABLE_H
---
+++ control_utils_lib/src/interpolation_table.c
@@ -0,0 +1,62 @@
+#include "interpolation_table.h"
+
+#include <stddef.h>
+
+bool control_utils_interpolation_table_init(control_utils_interpolation_table_t *const table) {
+    const bool ret = table != NULL;
+    if (ret) {
+        for (size_t i = 0; i < table->slopeBufferSize; i++) {
+            const float x_diff = table->x[i + 1] - table->x[i];
+            const float y_diff = table->y[i + 1] - table->y[i];
+            table->slopeBuffer[i] = y_diff / x_diff;
+        }
+
+        table->initialized = true;
+    }
+
+    return ret;
+}
+
+bool control_utils_interpolation_table_interpolate(control_utils_interpolation_table_t const *const table, float x,
+                                                   float *const y) {
+    bool ret = (table != NULL) && (y != NULL);
+    if (ret) {
+        ret &= table->initialized;
+    }
+
+    if (ret) {
+        size_t idx = 0;
+        float slope = 0;
+
+        bool found = false;
+
+        // Find the interval where x is located
+        for (size_t i = 0; i < table->size - 1; i++) {
+            if ((x >= table->x[i]) && (x < table->x[i + 1])) {
+                idx = i;
+                // Only assign slope here, as we will use the fact that if slope is not calculated here it will be 0, thereby only
+                // returning the y value
+                slope = table->slopeBuffer[i];
+                found = true;
+                break;
+            }
+        }
+
+        if (found == false) {
+            // only possible if x is outside the range of the table.
+            if (x <= table->x[0]) {
+                idx = 0;
+                slope = 0.0F;
+            } else {
+                idx = table->size - 1;
+                slope = 0.0F;
+            }
+        }
+
+        const float delta_x = x - table->x[idx];
+
+        *y = table->y[idx] + slope * delta_x;
+    }
+
+    return ret;
+}
